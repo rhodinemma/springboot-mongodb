04a7b8c65739db424b0731987d16a1bdc8a3cfa3
rhodinemma <rhodinemma10@gmail.com>
2023-01-30 23:26:09 +0300

    feat: add resource files

---
+++ src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.demo.model.Tutorial;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+    List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
+}
